03602aa852e17db82794e04a0d0dd0c08631d247
owoDra <85442566+owoDra@users.noreply.github.com>
2024-01-02 01:53:13 +0900

    Modifierの追加

---
+++ Source/GCExt/GCExt/Modifier/CharacterModifier_SetMesh.cpp
@@ -0,0 +1,55 @@
+﻿// Copyright (C) 2023 owoDra
+
+#include "CharacterModifier_SetMesh.h"
+
+#include "CharacterMeshAccessorInterface.h"
+#include "GCExtLogs.h"
+
+#include "GameFramework/Pawn.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Animation/AnimInstance.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(CharacterModifier_SetMesh)
+
+
+UCharacterModifier_SetMesh::UCharacterModifier_SetMesh()
+{
+	bOnlyApplyOnLocal = false;
+	bApplyOnClient = true;
+	bApplyOnServer = true;
+}
+
+
+bool UCharacterModifier_SetMesh::OnApply(APawn* Pawn) const
+{
+	const auto bCanApply{ Super::OnApply(Pawn) };
+
+	if (bCanApply)
+	{
+		for (const auto& MeshToSet : MeshesToSetMesh)
+		{
+			if (auto* Mesh{ ICharacterMeshAccessorInterface::Execute_GetMeshByTag(Pawn, MeshToSet.MeshTag) })
+			{
+				auto* LoadedSkeltalMesh
+				{
+					MeshToSet.SkeletalMesh.IsNull() ? nullptr :
+					MeshToSet.SkeletalMesh.IsValid() ? MeshToSet.SkeletalMesh.Get() : MeshToSet.SkeletalMesh.LoadSynchronous()
+				};
+
+				auto* LoadedAnimInstanceClass
+				{
+					MeshToSet.AnimInstance.IsNull() ? nullptr :
+					MeshToSet.AnimInstance.IsValid() ? MeshToSet.AnimInstance.Get() : MeshToSet.AnimInstance.LoadSynchronous()
+				};
+
+				UE_LOG(LogGCE, Log, TEXT("++SkeltalMesh (Name: %s)"), *GetNameSafe(LoadedSkeltalMesh));
+				UE_LOG(LogGCE, Log, TEXT("++AnimInstance (Name: %s)"), *GetNameSafe(LoadedAnimInstanceClass));
+
+				Mesh->SetSkeletalMesh(LoadedSkeltalMesh);
+				Mesh->SetAnimInstanceClass(LoadedAnimInstanceClass);
+			}
+		}
+	}
+
+	return bCanApply;
+}
---
+++ Source/GCExt/GCExt/Modifier/CharacterModifier_SetMesh.h
@@ -0,0 +1,55 @@
+﻿// Copyright (C) 2023 owoDra
+
+#pragma once
+
+#include "CharacterModifier.h"
+
+#include "GameplayTagContainer.h"
+
+#include "CharacterModifier_SetMesh.generated.h"
+
+class USkeletalMesh;
+class UAnimInstance;
+
+
+/**
+ * Entry data of Mesh to be changed
+ */
+USTRUCT(BlueprintType)
+struct FMeshToSetMesh
+{
+	GENERATED_BODY()
+public:
+	FMeshToSetMesh() {}
+
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FGameplayTag MeshTag;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TSoftObjectPtr<USkeletalMesh> SkeletalMesh{ nullptr };
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TSoftClassPtr<UAnimInstance> AnimInstance{ nullptr };
+
+};
+
+
+/**
+ * Modifier class to Set mesh for Pawn
+ */
+UCLASS(meta = (DisplayName = "CM Set Mesh"))
+class UCharacterModifier_SetMesh final : public UCharacterModifier
+{
+	GENERATED_BODY()
+public:
+	UCharacterModifier_SetMesh();
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Set Mesh")
+	TArray<FMeshToSetMesh> MeshesToSetMesh;
+
+protected:
+	virtual bool OnApply(APawn* Pawn) const override;
+
+};
