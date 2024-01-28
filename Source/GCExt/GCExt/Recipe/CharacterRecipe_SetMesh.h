238e837bd42c7021e2b66574bf0324c3b8211c00
owoDra <85442566+owoDra@users.noreply.github.com>
2024-01-28 23:08:28 +0900

    ファイルを分割

--- Source/GCExt/GCExt/Recipe/CharacterRecipe_SetMesh.h
+++ Source/GCExt/GCExt/Recipe/CharacterRecipe_SetMesh.h
@@ -4,59 +4,9 @@
 
 #include "Recipe/CharacterRecipe.h"
 
-#include "GameplayTagContainer.h"
+#include "Recipe/CharacterSetMeshTypes.h"
 
 #include "CharacterRecipe_SetMesh.generated.h"
-
-class USkeletalMesh;
-class UAnimInstance;
-
-
-/**
- * Entry data of Mesh to be changed
- */
-USTRUCT(BlueprintType)
-struct GCEXT_API FMeshToSetMesh
-{
-	GENERATED_BODY()
-public:
-	FMeshToSetMesh() {}
-
-public:
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (Categories = "MeshType"))
-	FGameplayTag MeshTag;
-
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (InlineEditConditionToggle))
-	bool bShouldChangeMesh{ false };
-
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (EditCondition = "bShouldChangeMesh"))
-	TSoftObjectPtr<USkeletalMesh> SkeletalMesh{ nullptr };
-
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (InlineEditConditionToggle))
-	bool bShouldChangeAnimInstance{ false };
-
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (EditCondition = "bShouldChangeAnimInstance"))
-	TSoftClassPtr<UAnimInstance> AnimInstance{ nullptr };
-
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (InlineEditConditionToggle))
-	bool bShouldChangeLocation{ false };
-
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (EditCondition = "bShouldChangeLocation"))
-	FVector NewLocation{ 0.0f, 0.0f, -90.0f };
-
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (InlineEditConditionToggle))
-	bool bShouldChangeRotation{ false };
-
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (EditCondition = "bShouldChangeRotation"))
-	FRotator NewRotation{ 0.0f, -90.0f, 0.0f };
-
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (InlineEditConditionToggle))
-	bool bShouldChangeScale{ false };
-
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (EditCondition = "bShouldChangeScale"))
-	FVector NewScale{ FVector::OneVector };
-
-};
 
 
 /**
